f0343e04cccf3977c46a3097140d225ec07533a0
James W <jamrobwill1234@yahoo.com>
2015-05-14 12:14:48 -0400

    Scratch Commit

--- ScratchDraw/ScratchDraw/ViewController.swift
+++ ScratchDraw/ScratchDraw/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBAction func clearScratches(sender: AnyObject) {
+        scratchPad.scratches = []
+        scratchPad.setNeedsDisplay()
+    }
+    
+    @IBOutlet weak var scratchPad:
+    ScratchView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //view.addSubview(scratchPad)
+        //scratchPad.frame = view.frame
+        //scratchPad.backgroundColor = UIColor.whiteColor()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        if let touch = touches.first as? UITouch {
+            
+            let location = touch.locationInView(scratchPad)
+            
+            scratchPad.newScratchWithStartPoint(location)
+        }
+        
+        
     }
-
-
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if let touch = touches.first as? UITouch {
+            let location = touch.locationInView(scratchPad)
+            //scratchPad.updateCurrentScratchLastPoint(location)
+            scratchPad.addPointToCurrentScratch(location)
+        }
+        
+    }
+    
+    @IBAction func changeColor(sender: UIButton) {
+        
+        scratchPad.currentColor = sender.backgroundColor!
+    }
+    
 }
 
